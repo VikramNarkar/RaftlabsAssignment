33e4885b75fd9ff62ffde18dcb595d4e165b5d4c
Vikram <vikram@joviantechnologies.io>
2025-06-19 20:29:06 +0530

    Created Models (Domain and Dtos needed for User data and Users List.

---
+++ ExternalProvider/Models/Dto/SupportDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalProvider.Models.Dto
+{
+    public class SupportDto
+    {
+        public string Url { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+    }
+}
---
+++ ExternalProvider/Models/Dto/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalProvider.Models.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string First_Name { get; set; } = string.Empty;
+        public string Last_Name { get; set;} = string.Empty;
+        public string Avatar { get; set; } = string.Empty;
+    }
+}
