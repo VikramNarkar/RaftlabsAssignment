7b790dd0faf5aa89abc309b766121debaa1ed928
Vikram <vikram@joviantechnologies.io>
2025-06-21 17:47:12 +0530

    Setting up a unit test project

---
+++ ExternalProviderTest/Services/ExternalUserServiceTests.cs
@@ -0,0 +1,11 @@
+namespace ExternalProviderTest.Services
+{
+    public class ExternalUserServiceTests
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
